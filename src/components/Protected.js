a380c0456f92c02c6af34acd31f75bb0056cb87a
shimeng <shimeng@coamc.com>
2017-12-27 10:19:36 +0800

    init

--- src/components/Protected.js
+++ src/components/Protected.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+/* import React, { Component } from "react";
 import { inject, observer } from "mobx-react";
 import { Redirect } from "react-router-dom";
 
@@ -29,4 +29,4 @@ export default function Protected(Component) {
 		}
 	}
 	return AuthenticatedComponent;
-}
+} */
